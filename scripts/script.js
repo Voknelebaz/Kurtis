c2c114627de5e65398fc50c7003c111730d94fe6
Max Zabelenkov <maxx.zabelenkov@gmail.com>
2023-09-12 11:53:22 +0300

    burger done

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 
 const burger = document.querySelector('.controls__burger');
 const menu__mobile = document.querySelector('.menu__mobile');
-
+const body = document.querySelector('.body');
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu__mobile.classList.toggle('active');
+    body.classList.toggle('unscroll');
 });
 
 
